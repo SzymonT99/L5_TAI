f1a381a668191260da093d6743f589bc0c9434f5
Szymon Tyrka <szy.tyrka@gmail.com>
2021-03-22 11:21:30 +0100

    init angular app and add library

--- script/complex.js
+++ script/complex.js
@@ -1,32 +1,32 @@
-"use strict";
-class Complex {
-    constructor(real, imaginary) {
+var Complex = /** @class */ (function () {
+    function Complex(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    add(obj) {
+    Complex.prototype.add = function (obj) {
         return new Complex(this.real + obj.real, this.imaginary + obj.imaginary);
-    }
-    subtract(obj) {
+    };
+    Complex.prototype.subtract = function (obj) {
         return new Complex(this.real - obj.real, this.imaginary - obj.imaginary);
-    }
-    module() {
+    };
+    Complex.prototype.module = function () {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    }
-    toString() {
+    };
+    Complex.prototype.toString = function () {
         console.log('RE: ', this.real, ', IM: ', this.imaginary);
-    }
-}
-let c1 = new Complex(3, 6);
+    };
+    return Complex;
+}());
+var c1 = new Complex(3, 6);
 console.log('c1:');
 c1.toString();
-let c2 = new Complex(4, 9);
+var c2 = new Complex(4, 9);
 console.log('c2:');
 c2.toString();
 console.log('suma c1 i c2');
-let c3 = c1.add(c2);
+var c3 = c1.add(c2);
 c3.toString();
 console.log('różnica wyniku sumy i c1');
-let c4 = c3.subtract(c1);
+var c4 = c3.subtract(c1);
 c4.toString();
 console.log('moduł dla wyniku różnicy: ', c4.module());
